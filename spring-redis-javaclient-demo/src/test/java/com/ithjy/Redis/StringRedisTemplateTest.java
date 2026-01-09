bcb33fa0d2ceac734ff1fa9fe58bf849b050be51
hjySaver <1357826695@qq.com>
2026-01-10 01:04:59 +0800

    test

--- spring-redis-javaclient-demo/src/test/java/com/ithjy/Redis/StringRedisTemplateTest.java
+++ spring-redis-javaclient-demo/src/test/java/com/ithjy/Redis/StringRedisTemplateTest.java
@@ -1,4 +1,8 @@
 package com.ithjy.Redis;
 
+import org.springframework.data.redis.core.StringRedisTemplate;
+
 public class StringRedisTemplateTest {
+    StringRedisTemplate stringRedisTemplate=new StringRedisTemplate();
+    
 }
